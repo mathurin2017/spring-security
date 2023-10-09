b14eecdd55f60b7a320da396eadc79453f3a6b47
Mathurin BALACOUNE <mathurin.balacoune@gmail.com>
2023-10-09 14:38:52 +0200

    init commit

---
+++ src/main/java/com/lloufa/springsecurity/entity/Role.java
@@ -0,0 +1,5 @@
+package com.lloufa.springsecurity.entity;
+
+public enum Role {
+    USER, ADMIN;
+}
---
+++ src/test/java/com/lloufa/springsecurity/SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lloufa.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
